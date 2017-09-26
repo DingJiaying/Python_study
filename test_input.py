7d5dc47fb4ba44d7ae0d5708b8599f9e4a2cf806
Ding Jiaying <18305186709@163.com>
2017-09-26 21:46:43 +0800

    Change3 in 0926

---
+++ test_input.py
@@ -0,0 +1,8 @@
+a_input = input('please give a number:') #可以定义一个输入的数值类型
+#print("This input number is:",a_input)
+if a_input=='1':
+    print("This is a good one")
+elif a_input==str(2):
+    print("See you next time")
+else:
+    print("Good luck")
